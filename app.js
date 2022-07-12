5f4977bc14493a7ab318c912126605f496913148
DenizShayan <92824050+DenizShayan@users.noreply.github.com>
2022-07-13 00:42:48 +0430

    3

---
+++ app.js
@@ -0,0 +1,45 @@
+const scoreDisply = document.getElementById('score-display')
+const questionDisplay = document.getElementById('question-display')
+
+const questions =[
+    {
+        correct: 2,
+        option: ['jury', 'assess'],
+        quiz: ['value', 'estimate', 'evaluate'],
+      },
+      {
+        correct: 2,
+        option: ['trace', 'adjacent'],
+        quiz: ['close', 'near', 'next'],
+      },
+      {
+        correct: 2,
+        option: ['mad', 'exotic'],
+        quiz: ['foreign', 'national', 'ethnic'],
+      },
+      {
+        correct: 1,
+        option: ['forecast', 'sustainable'],
+        quiz: ['assume', 'insight', 'weather'],
+      },
+      {
+        correct: 2,
+        option: ['charity', 'rapid'],
+        quiz: ['fast', 'quick', 'prompt'],
+      },
+    {
+        quiz:['value', 'estimate', 'evaluate'],
+        options: ['jury', 'assess'],
+        correct: 2
+    },
+    {
+        quiz:['value', 'estimate', 'evaluate'],
+        options: ['jury', 'assess'],
+        correct: 2
+    },
+    {
+        quiz:['value', 'estimate', 'evaluate'],
+        options: ['jury', 'assess'],
+        correct: 2
+    }
+]
